ea753fec1e06db6034b02e35ca8d08db547e49e5
Tylar <murray.tylar@gmail.com>
2020-01-02 12:31:59 -0500

    + function & readlines usage instead

--- bb3_site_separator_revised.py
+++ bb3_site_separator_revised.py
@@ -8,6 +8,26 @@ def ensure_dir(file_path):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+def get_next_line(line_of_text):
+    line_fmt = (
+        '{:2d}/{:2d}/{:2d}	{:2d}:{:2d}:{:2d}	'
+        '{:3d}	{:4d}	{:3d}	{:4d}	{:3d}	{:4d}	{:3d}'
+    )
+    parsed_line = parse(line_fmt, line_of_text)
+        # this checks if the date is in correct format, assumes data will be correct if date is
+        time_of_sample = "{}/{}/{} {}:{}:{}".format(
+            parsed_line[0], parsed_line[1], parsed_line[2],
+            parsed_line[3], parsed_line[4], parsed_line[5]
+        )
+        dt = datetime.strptime(time_of_sample, dt_fmt)
+
+        # if open dt_prev, will take current dt and subtract 1 sec
+        if dt_prev == "":
+            dt_prev = dt - timedelta(seconds=1)
+        dt_current = dt
+        
+        return time_of_sample, dt_current, dt_prev
+        
 # folder_name = input("what do you want directory to be?") + '/'     # commment in if want to customize folder
 folder_name = 'BB3/'        # comment out if want to costumize folder
 ensure_dir(folder_name)
@@ -17,10 +37,7 @@ dt_prev = ""
 #will edit out when wanting to put site in at same time
 site = 0
 file_name = ''
-line_fmt = (
-    '{:2d}/{:2d}/{:2d}	{:2d}:{:2d}:{:2d}	'
-    '{:3d}	{:4d}	{:3d}	{:4d}	{:3d}	{:4d}	{:3d}'
-)
+
 # edit in if want to choose file name with extension
 # sample_file_name = input('Whats the file name?')
 # can edit out before running anything if want to type as an input
@@ -30,15 +47,15 @@ f = open(sample_file_name,"r")
 # use readline() to read the first line
 line_of_text = f.readline()
 
+# lines = f.readlines()
+# for line_n in len():
+#     this_line = lines[line_n]
+#     next_line = lines[line_n+1]
+#     prev_line = lines[line_n-1]
 while line_of_text:
-        parsed_line = parse(line_fmt, line_of_text)
-        # this checks if the date is in correct format, assumes data will be correct if date is
+        
         try:
-            time_of_sample = "{}/{}/{} {}:{}:{}".format(
-                parsed_line[0], parsed_line[1], parsed_line[2],
-                parsed_line[3], parsed_line[4], parsed_line[5]
-            )
-            dt = datetime.strptime(time_of_sample, dt_fmt)
+            time_of_sample, dt_current, dt_prev = get_next_line(line_of_text)
         except Exception:
             print(time_of_sample)
             # puts error lines into a file
@@ -46,11 +63,6 @@ while line_of_text:
             error_file.write(line_of_text)
             line_of_text = f.readline()
             continue
-
-        # if open dt_prev, will take current dt and subtract 1 sec
-        if dt_prev == "":
-            dt_prev = dt - timedelta(seconds=1)
-        dt_current = dt
 
         # checks to make sure time is not negative, would mean error
         if dt_current - dt_prev < timedelta(milliseconds=0):
@@ -73,15 +85,20 @@ while line_of_text:
                 newfile = open(folder_name + file_name, "a")
                 newfile.write(line_of_text)
         else:
-            dt_prev = dt_current
-            i = dt_current.strftime("%d_%m_%yT%H%M%S")
-            # this is so can look up timestamp on spreadsheet and label site
-            print('Old time: ' + str(dt_current))
-            # edit in later, to name site during run
-            # site = input("What is the site ID?")
-            site = site + 1         #edit out later
-            file_name = 'MBON_{}_site_{}.txt'.format(i,site)
-            newfile = open(folder_name + file_name, "a")
-            newfile.write(line_of_text)
+            # TODO: check if the next timedelta(s) are < 0
+            u_inp = input('do you want to start a new file?')
+            if u_inp == 'y':
+                dt_prev = dt_current
+                i = dt_current.strftime("%d_%m_%yT%H%M%S")
+                # this is so can look up timestamp on spreadsheet and label site
+                print('Old time: ' + str(dt_current))
+                # edit in later, to name site during run
+                # site = input("What is the site ID?")
+                site = site + 1         #edit out later
+                file_name = 'MBON_{}_site_{}.txt'.format(i,site)
+                newfile = open(folder_name + file_name, "a")
+                newfile.write(line_of_text)
+            else:
+                print('skipping line')
         line_of_text = f.readline()
 f.close()
